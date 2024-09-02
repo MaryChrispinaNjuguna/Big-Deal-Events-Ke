e3d2c5ac0e47dd5eeff4299f34c6f4adf9827b52
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-09-02 23:49:40 +0300

    static files

--- events/website/views.py
+++ events/website/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.core.mail import send_mail, BadHeaderError
 
 def index (request):
     return render (request, 'index.html')
