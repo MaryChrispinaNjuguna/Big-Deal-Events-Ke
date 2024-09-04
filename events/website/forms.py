dff2618a6f1dd3cff6a9c8b47f3223b9bd4640be
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-09-04 21:46:07 +0300

    favicon

---
+++ events/website/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}))
+    email = forms.EmailField(max_length=100, required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}))
+    subject = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'}))
+    message = forms.CharField(required=True, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Message', 'rows': 6}))
